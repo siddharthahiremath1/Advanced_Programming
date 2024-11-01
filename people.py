eddf6d371320f12e47accec3dced10b7e0812618
siddharthahiremath1 <shiremath@davidsonacademy.unr.edu>
2024-11-01 10:29:34 -0700

    testing pull requests

--- people.py
+++ people.py
@@ -1,4 +1,15 @@
 class Person():
-    def __init__(self, name, age, height):
+    def __init__(self, name:str, age:int, height:int):
         self.name = name
         self.age = age
+        self.height = height
+    def loginfo(self):
+        print('Name:')
+        print(self.name)
+        print('Age:')
+        print(self.age)
+        print('Height:')
+        print(self.height)
+
+class Student(Person):
+    def __init__(self, classes:list, )
